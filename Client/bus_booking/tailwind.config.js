9fb17c05361c60965d7a8d2f519d3ba01e2c8d02
Sonu Yadav <sy239370@gmail.com>
2026-06-25 13:03:24 +0530

    Bus Booking app initialized frontend

---
+++ Client/bus_booking/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js, jsx, ts, tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        okra: [
+          'Okra-regular',
+          'Okra-medium',
+          'Okra-extrabold',
+          'Okra-bold',
+          'Okra-mediumLight',
+        ],
+      },
+      colors: {
+        primary: '#FC5431',
+        secondary: '#FDBB8A',
+        tertiary: '#CF3239',
+      },
+    },
+  },
+  plugins: [],
+};
